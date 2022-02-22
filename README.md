8a70673e67a832e52129419c8b6f38c3b71e3077
Daniel Kott <danielkoett@gmail.com>
2022-02-22 10:14:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Weather App
+## Api Practice using AccuWeather's Api to fetch weather information
+
+Just a simple weather app, that allows a user to enter a location and the corresponding temperature will be displayed. 
+
+The main focus was not on the design, but for me to practice fetching data from an external source. In this case the AccuWeather Api was used to get weather information on a specific location.
+
+![](img/screenshot.png)
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+### What I learned
+
+- Practicing JS promises using async + await
+- Using async functions to fetch the data from Accuweather's api.
+- Adding the query parameters
+- DOM manipulation to update the UI 
+
---
+++ scripts/forecast.js
@@ -0,0 +1,33 @@
+const key = 'Z4SwmmE8PbQgXxmrU1Jy0OzbUYFnWimp'; //api key
+
+//get weather information
+const getWeather = async (cityId) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${cityId}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+//get city information
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// getCity('schindellegi').then(data => {
+//     return getWeather(data.Key)
+// }).then(data => {
+//     console.log(data);
+// }).catch(error => console.log(error));
+
